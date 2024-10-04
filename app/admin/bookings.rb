8ba54e5f0bcb9d47fde3c9d7b822d1bbf446e7af
Akshay Bhadoriya <abhadoriya@bestpeers.com>
2024-10-04 19:02:24 +0530

    displaying flash error and admin creation

---
+++ app/admin/bookings.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Booking do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :check_in_date, :check_out_date, :check_in_time, :check_out_time, :number_of_rooms, :type_of_room, :status, :user_id, :room_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:check_in_date, :check_out_date, :check_in_time, :check_out_time, :number_of_rooms, :type_of_room, :status, :user_id, :room_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,45 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_and_belongs_to_many :hotels
-  has_many :bookings 
+  has_many :bookings
   has_many :rooms, through: :bookings
-  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable 
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  validate :no_overlapping_bookings
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at email encrypted_password id remember_created_at reset_password_sent_at
+       reset_password_token updated_at]
+  end 
 
-private
-       
-def no_overlapping_bookings
-  bookings.each do |booking|
-    if booking.overlapping_bookings.exists?
-         errors.add(:base, 'User cannot have overlapping bookings.')
-    end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "location", "name", "updated_at"]
   end
-end
 
+  def self.ransackable_associations(auth_object = nil)
+    ["rooms", "bookings"]  # Adjust based on your actual associations
+  end
+
+  # def self.ransackable_associations(_auth_object = nil)
+  #   %w[bookings hotels rooms]
+  # end
+
+  # def self.ransackable_attributes(_auth_object = nil)
+  #   %w[created_at id location name updated_at]
+  # end
+
+  # def self.ransackable_associations(auth_object = nil)
+  #   ["bookings", "hotels", "rooms"]
+  # end
+  #   validate :no_overlapping_bookings
+
+  # private
+
+  # def no_overlapping_bookings
+  #   bookings.each do |booking|
+  #     if booking.overlapping_bookings.exists?
+  #          errors.add(:base, 'User cannot have overlapping bookings.')
+  #     end
+  #   end
+  # end
 end
