ac258940cbec67578e04b777af73b170aed27d09
Akshay Bhadoriya <abhadoriya@bestpeers.com>
2024-10-08 19:22:39 +0530

    implemented check_rooms_availability action in bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,56 +1,40 @@
 class BookingsController < ApplicationController
-  before_action :find_resource, only: %i[new create]
-  # before_action :find_resource, only: :create
+  # before_action :validity_params, only: :new
+  before_action :find_resource, only: %i[create new]
+
+  def availability
+    check_in = params[:check_in_date]
+    check_out = params[:check_out_date]
+    @available_rooms = @rooms.select do |room|
+      room.bookings.none? do |booking|
+        # Check for overlapping dates
+        (booking.check_in_date < check_out) && (booking.check_out_date > check_in) && (booking.user_id == current_user.id)
+      end
+    end
+  end
 
   def new
-    # @booking = @rooms.bookings.new
-    # @rooms = Room.where(hotel_id: params[:hotel_id])
     @booking = Booking.new
+    @hotel = Hotel.find(params[:hotel_id]) # Ensure you fetch the hotel
+    @rooms = @hotel.rooms.all
+
+    # # Fetch check_in and check_out from params, default to nil if not present
+    # check_in = params[:check_in_date]
+    # check_out = params[:check_out_date]
+
+    # @available_rooms = @rooms.select do |room|
+    #   room.bookings.none? do |booking|
+    #     # Check for overlapping dates
+    #     (booking.check_in_date < check_out) &&
+    #     (booking.check_out_date > check_in) &&
+    #     (booking.user_id == current_user.id)
+    #   end
+    # end
   end
 
   def index
     @bookings = current_user.bookings.all
   end
-
-  # def edit
-  #   @booking = current_user.bookings.find(params[:id])
-  #   room = Room.find_by!(id: @booking.room_id)
-  #   check_in_date = .check_in_date
-  #   check_out_date = @booking.check_out_date
-
-  #    hotel_id = Room.find_by!(id:@booking.room_id).hotel_id
-  #    hotel = Hotel.find_by!(id:hotel_id)
-  #    @rooms = hotel.rooms
-
-  #   if @booking.update(booking_params)
-  #     redirect_to bookings_path, notice: 'Booking was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def update
-  #   # @booking = current_user.bookings.find(params[:id])
-  #   if
-  #   if @booking.update(booking_params)
-  #     redirect_to bookings_path, notice: 'Booking was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   @booking.user = current_user
-
-  #   if @booking.save
-  #     @room = Room.where(room_id: room_id)
-  #     @room.status = "Booked"
-  #     redirect_to @booking, notice: 'Booking was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
 
   def create
     ActiveRecord::Base.transaction do
@@ -59,8 +43,8 @@ class BookingsController < ApplicationController
 
       if @booking.save
         # byebug
-        @room = Room.find(@booking.room_id)  
-        hotel = Hotel.find_by(id:@room.hotel_id) 
+        @room = Room.find(@booking.room_id)
+        hotel = Hotel.find_by(id: @room.hotel_id)
         @booking.update!(number_of_rooms: 1)
         # current_user.hotels << hotel
         # @type = @room.room_type
@@ -96,12 +80,57 @@ class BookingsController < ApplicationController
   def find_resource
     # @rooms = Rooms.where(params[:resource_id])
     @rooms = Room.where(hotel_id: params[:hotel_id])
-    # byebug
+    @hotel = Hotel.find_by(id: params[:hotel_id])
+    byebug
     # if rooms .
   end
 
-  def booking_params
-    # byebug
-    params.require(:booking).permit(:check_in_date, :check_out_date, :check_in_time, :check_out_time, :room_id)
+  # def booking_params
+  #   # byebug
+  #   params.require(:booking).permit(:check_in_date, :check_out_date, :check_in_time, :check_out_time, :room_id)
+  # end
+
+  def validity_params
+    params.require(:room).permit(:check_in_date, :check_out_date)
   end
 end
+
+# def edit
+#   @booking = current_user.bookings.find(params[:id])
+#   room = Room.find_by!(id: @booking.room_id)
+#   check_in_date = .check_in_date
+#   check_out_date = @booking.check_out_date
+
+#    hotel_id = Room.find_by!(id:@booking.room_id).hotel_id
+#    hotel = Hotel.find_by!(id:hotel_id)
+#    @rooms = hotel.rooms
+
+#   if @booking.update(booking_params)
+#     redirect_to bookings_path, notice: 'Booking was successfully updated.'
+#   else
+#     render :edit
+#   end
+# end
+
+# def update
+#   # @booking = current_user.bookings.find(params[:id])
+#   if
+#   if @booking.update(booking_params)
+#     redirect_to bookings_path, notice: 'Booking was successfully updated.'
+#   else
+#     render :edit
+#   end
+# end
+
+# def create
+#   @booking = Booking.new(booking_params)
+#   @booking.user = current_user
+
+#   if @booking.save
+#     @room = Room.where(room_id: room_id)
+#     @room.status = "Booked"
+#     redirect_to @booking, notice: 'Booking was successfully created.'
+#   else
+#     render :new
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "homes#index"
+  root 'homes#index'
 
   devise_scope :user do
-    get 'users/sign_out' => "devise/sessions#destroy"
-  end 
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 
-  resources :hotels do  
-    
-    collection do 
+  get 'bookings/availability', as: :availability_bookings
+
+  resources :hotels do
+    collection do
       get 'search'
     end
     resources :rooms, shallow: true do
-      resources :bookings, shallow: true
+      resources :bookings
     end
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: %i[index new create]
   end
-   resources :bookings
+
+  resources :bookings
 
   # resources :bookings do
   #   member do
