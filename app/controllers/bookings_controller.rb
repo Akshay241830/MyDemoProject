4b23f396597021803899593b888772156477380e
Akshay Bhadoriya <abhadoriya@bestpeers.com>
2024-10-14 20:28:03 +0530

    implemented booking of rooms and performed show and cancel booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,83 +14,125 @@ class BookingsController < ApplicationController
     # end
   end
 
-  def check_availability 
+  def check_availability
     byebug
     @hotel = Hotel.find(params[:hotel_id])
-
-    @check_in = Date.parse(params[:check_in_date])
-
-    @check_out = Date.parse(params[:check_out_date])
-
+    # @check_in = Date.parse(params[:check_in_date])
+    # @check_out = Date.parse(params[:check_out_date])
+    @check_in = params[:check_in_date]
+    @check_out = params[:check_out_date]
     @rooms = @hotel.rooms
 
-    if @check_in < Date.today
-      flash[:alert] = 'Check in cannot be in past'
-    elsif @check_out < Date.today
-      flash[:alert] = 'Check out cannot be in past'
-    else
+    if Date.parse(@check_in) < Date.today
+      flash[:alert] = 'Check in cannot be in the past'
+      redirect_to hotel_bookings_path(@hotel) and return
+    elsif Date.parse(@check_out) < Date.today
+      flash[:alert] = 'Check out cannot be in the past'
+      redirect_to hotel_bookings_path(@hotel) and return
+    end
 
-      # @available_rooms = @rooms.select do |room|
-      #   room.bookings.where("tsrange(check_in_date, check_out_date, '[]') && tsrange(?, ?, '[]')", check_in, check_out).empty?
-      # end
-      @available_rooms = @rooms.select do |room|
-        room.bookings.where('(check_in_date < ? AND check_out_date > ?) OR (check_in_date = ? AND check_out_date = ?)',
-                            @check_out, @check_in, @check_in, @check_out).empty?
+    @available_rooms = @rooms.select do |room|
+      room.bookings.where(
+        '(check_in_date < ? AND check_out_date > ?) OR
+         (check_in_date = ? AND check_out_date = ?) OR
+         (check_in_date = ? OR check_out_date = ?)',
+        Date.parse(@check_out),
+        Date.parse(@check_in),
+        Date.parse(@check_in),
+        Date.parse(@check_out),
+        Date.parse(@check_in),
+        Date.parse(@check_out)
+      ).empty?
+    end
+
+    # Store available room IDs and check-in/check-out dates in session
+    session[:available_rooms] = @available_rooms.map(&:id)
+    session[:check_in] = @check_in
+    session[:check_out] = @check_out
+
+    # Redirect to new booking action
+    redirect_to new_hotel_booking_path(@hotel.id)
+  end
+
+  # def index
+  #   @bookings = current_user.bookings.all
+  # end
+
+  def new
+    @booking = Booking.new
+    @hotel = Hotel.find(params[:hotel_id])
+    byebug
+    if session[:available_rooms].present?
+      @available_rooms = Room.where(id: session[:available_rooms])
+      # @check_in = Date.parse(session[:check_in])
+      # @check_out = Date.parse(session[:check_out])
+
+      @check_in = session[:check_in]
+      @check_out = session[:check_out]
+
+      # Clear the session after use
+      session.delete(:available_rooms)
+      session.delete(:check_in)
+      session.delete(:check_out)
+    else
+      @available_rooms = [] # No available rooms found
+      @check_in = nil
+      @check_out = nil
+    end
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      byebug
+      @booking = Booking.new(booking_params)
+      @booking.user = current_user
+
+      byebug
+      if @booking.save
+        # byebug
+        @room = Room.find(@booking.room_id)
+        hotel = Hotel.find_by(id: @room.hotel_id)
+        # current_user.hotels << hotel
+        # @type = @room.room_type
+        @booking.update!(type_of_room: @room.room_type)
+        @booking.update!(number_of_rooms: 1)
+        # @room.update!(status: "Booked")
+
+        redirect_to hotel_bookings_path, notice: 'Booking was successfully created.'
+      else
+        flash.now[:alert] = 'There was an error creating your booking.'
+        render :new, status: :unprocessable_entity
       end
-      # byebug
-      # render :availability, locals: { available_rooms: @available_rooms }
-      render 'bookings/check_availability'
-      # render 'bookings/custom_availability_view'
+    rescue ActiveRecord::RecordInvalid
+      byebug
+      flash[:alert] = 'Booking could not be created.'
+      render :new
     end
   end
 
   def index
     @bookings = current_user.bookings.all
-  end
-
-  def new 
     byebug
-    @booking = Booking.new
-  end
-
-  def create
-    ActiveRecord::Base.transaction do
-      @booking = Booking.new(booking_params)
-      @booking.user = current_user
-
-      if @booking.save
-        # byebug
-        @room = Room.find(@booking.room_id)
-        hotel = Hotel.find_by(id: @room.hotel_id)
-        @booking.update!(number_of_rooms: 1)
-        # current_user.hotels << hotel
-        # @type = @room.room_type
-        @booking.update!(type_of_room: @room.room_type)
-        @booking.number_of_rooms = 1
-        # @room.update!(status: "Booked")
-
-        redirect_to bookings_path, notice: 'Booking was successfully created.'
-      else
-        flash.now[:alert] = 'There was an error creating your booking.'
-        # render :new, status: :unprocessable_entity 
-        redirect_to availability_bookings_path, status: :unprocessable_entity
-      end
-    rescue ActiveRecord::RecordInvalid
-      # byebug
-      flash[:alert] = 'Booking could not be created.'
-      redirect_to availability_bookings_path
-    end
   end
 
   def destroy
-    # byebug
-    @booking = current_user.bookings.find(params[:id])
+    # @hotel = Hotel.find(params[:hotel_id]) # Find the hotel by ID
+    @booking = Booking.find(params[:id]) # Find the booking by ID
+    @room = Room.find(@booking.room_id)
+    @hotel = Hotel.find(@room.hotel_id)
     @booking.update!(status: 'Cancelled')
-    redirect_to bookings_path, notice: 'Booking was successfully canceled.'
+
+    redirect_to hotel_bookings_path(@hotel.id), notice: 'Booking was successfully canceled.'
+
+    # byebug
+    # @booking = current_user.bookings.find(params[:id])
+    # @booking.update!(status: 'Cancelled')
+    # redirect_to bookings_path, notice: 'Booking was successfully canceled.'
   end
 
   def show
-    @booking = current_user.bookings.find(params[:id])
+    # @hotel = Hotel.find(params[:hotel_id]) # Find the hotel by ID
+    @booking = Booking.find(params[:id]) # Find the booking by ID
   end
 
   private
@@ -98,22 +140,25 @@ class BookingsController < ApplicationController
   def find_resource
     # @rooms = Rooms.where(params[:resource_id])
     @rooms = Room.where(hotel_id: params[:hotel_id])
-    @hotel = Hotel.find_by(id: params[:hotel_id])
+
     # byebug
+    # if rooms .
   end
 
   def booking_params
-    
-    # params.require(:booking).permit(:check_in_date, :check_out_date, :check_in_time, :check_out_time, :room_id) 
-    params.require(:booking).permit(:check_in_date, :check_out_date, :room_id)
-    byebug
-  end
-
-  def validity_params
-    params.require(:room).permit(:check_in_date, :check_out_date)
+    params.require(:booking).permit(:check_in_date, :check_out_date, :room_id).tap do |booking_params|
+      if booking_params[:check_in_date].present?
+        booking_params[:check_in_date] =
+          Date.parse(booking_params[:check_in_date])
+      end
+      if booking_params[:check_out_date].present?
+        booking_params[:check_out_date] =
+          Date.parse(booking_params[:check_out_date])
+      end
+      booking_params[:room_id] = booking_params[:room_id].to_i if booking_params[:room_id].present?
+    end
   end
 end
-
 # def edit
 #   @booking = current_user.bookings.find(params[:id])
 #   room = Room.find_by!(id: @booking.room_id)
